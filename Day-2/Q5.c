2f635ba99fde318c7369ff64334c7bda0df9b18d
riyasinghriya983580 <riyasinghriya983580@gmail.com>
2026-06-05 14:06:26 +0530

    Add Day-2 C programs

---
+++ Day-2/Q5.c
@@ -0,0 +1,20 @@
+ //Q5. Find Sum of Digits of a Number
+ 
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n > 0) {
+        digit = n % 10;
+        sum = sum + digit;
+        n = n / 10;
+    }
+
+    printf("Sum of digits = %d", sum);
+
+    return 0;
+}

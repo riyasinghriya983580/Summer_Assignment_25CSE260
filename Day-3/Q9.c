129c555ebaa45a22888fae73dcb9f24fcfb92b7f
riyasinghriya983580 <riyasinghriya983580@gmail.com>
2026-06-06 20:09:45 +0530

    Day-3 uploaded.

---
+++ Day-3/Q9.c
@@ -0,0 +1,27 @@
+//Q9. Check Whether a Number is Prime
+#include <stdio.h>
+
+int main() {
+    int n, i, flag = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n <= 1) {
+        flag = 0;
+    } else {
+        for(i = 2; i < n; i++) {
+            if(n % i == 0) {
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    if(flag)
+        printf("Prime Number");
+    else
+        printf("Not a Prime Number");
+
+    return 0;
+}

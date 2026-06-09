86ea338ed9db907cf59ee7c6ad6b1d57cefbf0b7
riyasinghriya983580 <riyasinghriya983580@gmail.com>
2026-06-09 18:54:17 +0530

    Day-7 uploaded.

---
+++ Day-7/Q28.c
@@ -0,0 +1,23 @@
+//Q28. Recursive Reverse Number
+#include <stdio.h>
+
+int reverse = 0;
+
+int reverseNumber(int n) {
+    if(n == 0)
+        return reverse;
+
+    reverse = reverse * 10 + (n % 10);
+    return reverseNumber(n / 10);
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Reversed Number = %d", reverseNumber(n));
+
+    return 0;
+}

1254bd353aea458ed6b5a84cccf9385f6e2373dc
riyasinghriya983580 <riyasinghriya983580@gmail.com>
2026-06-10 20:18:56 +0530

    Day-8 uploaded.

---
+++ Day-8/Q31.c
@@ -0,0 +1,19 @@
+// Q31. Print Character Triangle
+
+#include <stdio.h>
+
+int main() {
+    int rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for(int i = 1; i <= rows; i++) {
+        for(char ch = 'A'; ch < 'A' + i; ch++) {
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

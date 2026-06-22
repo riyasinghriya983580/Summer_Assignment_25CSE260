1fc9221094149c78f887c3c166cc759ff727b0ea
riyasinghriya983580 <riyasinghriya983580@gmail.com>
2026-06-22 21:21:49 +0530

    Day-19 uploaded.

---
+++ Day-19/Q73.c
@@ -0,0 +1,33 @@
+// Q73. Add Matrices
+
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], c[10][10], r, col;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &col);
+
+    printf("Enter first matrix:\n");
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < col; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter second matrix:\n");
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < col; j++)
+            scanf("%d", &b[i][j]);
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < col; j++)
+            c[i][j] = a[i][j] + b[i][j];
+
+    printf("Result Matrix:\n");
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < col; j++)
+            printf("%d ", c[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
